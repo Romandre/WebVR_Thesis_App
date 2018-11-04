dfbcf60c230909c55c0113af695cc7ac0c723bb9
Roman <c1kuro00@students.oamk.fi>
2018-11-04 22:43:36 +0200

    Changed approach for pick function; added tock function to update balls position; optimized and improved scripts; cleaned premitives html definitions

--- src/script/ball.js
+++ src/script/ball.js
@@ -1,98 +1,104 @@
-/*
-var geometry = new THREE.SphereGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-var sphere = new THREE.Mesh( geometry, material );
-document.querySelector('a-scene').object3D.add( sphere );
+/**
+  *
+  * TODO: 
+  * 1) Implement throwing function 
+  * 2) Animate camera to imitate players breath
+  * 3) Add sound effects 
+  */
 
-object.position.copy( camera.position );
-object.rotation.copy( camera.rotation );
-object.updateMatrix();
-object.translateZ( - 10 );
-
-var boxEl = document.querySelector('a-sphere');
-	boxEl.addEventListener('mouseenter', function () {
-	boxEl.setAttribute('scale', {x: 2, y: 2, z: 2});
-});
-*/
-
-
-var cameraPosition;
-var worldPos = new THREE.Vector3();
-
+  //*******************************//
+ //** Register camera component **//
+//*******************************//
 AFRAME.registerComponent('#camera', {
 	init: function (oldData) {
-		cameraPosition = this.el.object3D.position;
-	    this.el.object3D.visible = this.data;
+		el = this.el.object3D;
+	    el.visible = this.data;
+  	},
+  	tock: function(time, timeDelta, evt) {
+  		el = this.el.object3D;
+  		el.rotation.set( 
+	  		el.rotation.x, 
+	  		el.rotation.y + Math.sin(time*0.002)*0.01, 
+	  		el.rotation.z
+	  	);
   	}
 });
 
-/*** TODO: 
-	1) Set proper position to a ball after dropping it;
-	2) Fix 'pick up ball' function for proper picking up. 
-***/
+
+var keepBallAttached = false;
+
+function attachBallToCamera(ballElement, cameraPos, cameraRot, interPoint, time, timeDelta) {
+	var ballObject = ballElement.object3D;
+
+	// Calculate distance between camera and the ball
+  	var dx = cameraPos.x - ballObject.position.x; 
+  	var dy = cameraPos.y - ballObject.position.y; 
+  	var dz = cameraPos.z - ballObject.position.z; 
+  	var distance = dx*dx+dy*dy+dz*dz;
+
+  	if (distance < 6) {
+  		ballElement.removeAttribute('dynamic-body');
+	  	ballObject.position.set( 
+	  		cameraPos.x - Math.sin(cameraRot.y) * 0.5, 
+	  		cameraPos.y + Math.sin(cameraRot.x)/4 - 0.2 + Math.sin(time*0.002)*0.01, 
+	  		cameraPos.z - Math.cos(cameraRot.y) * 0.5
+	  	);
+	  	ballObject.rotation.set( 
+	  		cameraRot.x - Math.PI, 
+	  		cameraRot.y/3, 
+	  		cameraRot.z
+	  	);
+    }
+
+    /**
+     * Logs with important info:
+     * 
+  	console.log('Camer position: ', cameraPos );
+  	console.log('Camer rotation: ', cameraRot );
+  	console.log('Distance: ', distance );
+  	console.log('I was clicked at: ', interPoint);
+  	console.log('Element is: ', ballObject.el.id);
+  	console.log('Element position: ', ballObject.position , '\n\n\n\n'); 
+  	**/	
+}
 
     
-  //**************************************//
- //** Component to listen for a click. **//
-//**************************************//
+  //*****************************//
+ //** Register ball component **//
+//*****************************//
 AFRAME.registerComponent('ball-events', {
 	init: function () {
 	  	el = this.el;
 	  	el.object3D.position.set(0, 2.150, -1.76);
-
-
-	  	  /**----------------------**/
-	  	 /* Pick up the ball event */
-	  	/**----------------------**/
-	    el.addEventListener('mousedown', function (evt) {
-	  		/*this.position.set( cameraPosition );
-		  	this.rotation.copy( cameraPosition );
-		  	this.updateMatrix();
-		  	this.translateZ( - 10 );*/
-
+	  	  
+	  	 /** Pick up the ball event **/
+	    el.addEventListener('mousedown', function (evt) {		 
 		  	var camera = document.querySelector('#camera').object3D;
 		  	var cameraPos = camera.position;
 		  	var cameraRot = camera.rotation;
-
-		  	var dx = cameraPos.x - el.object3D.position.x; 
-		  	var dy = cameraPos.y - el.object3D.position.y; 
-		  	var dz = cameraPos.z - el.object3D.position.z; 
-		  	var distance = dx*dx+dy*dy+dz*dz;
- 
-		  	if (distance < 6) {
-		  		el.removeAttribute('dynamic-body');
-			  	el.object3D.position.set( 
-			  		cameraPos.x - Math.sin(cameraRot.y) * 0.6, 
-			  		cameraPos.y - 1.5, 
-			  		cameraPos.z - Math.cos(cameraRot.y) * 0.6
-			  	);
-			  	el.object3D.rotation.set( 
-			  		cameraRot.x, 
-			  		cameraRot.y, 
-			  		cameraRot.z
-			  	);
-		    }
-		  	//el.setAttribute('position', {x:pos.x, y:pos.y, z:pos.z-1});
-		  	//el.setAttribute('dynamic-body', '');
-
-		  	console.log('Camer position: ', cameraPos );
-		  	console.log('Camer rotation: ', cameraRot );
-		  	console.log('Distance: ', distance );
-		  	console.log('I was clicked at: ', evt.detail.intersection.point);
-		  	console.log('Element is: ', el.object3D.el.id);
-		  	console.log('Element position: ', el.object3D.position , '\n\n\n\n');		  	
+		  	var interPoint = evt.detail.intersection.point;
+		  	attachBallToCamera(el, cameraPos, cameraRot, interPoint, null, null);	
+			keepBallAttached = true;
 	    });
-
-
-	      /**-------------------**/
-		 /* Drop the ball event */
-		/**-------------------**/
-	    window.addEventListener("keyup", (e) => {
-        	if (e.keyCode == 32) {
+	    
+		 /** Drop the ball event **/
+    	window.addEventListener("keyup", (e) => {
+    		/* If space is tapped */
+        	if (e.keyCode == 32 ) {
           		el.setAttribute('dynamic-body', '');
-				var newElPos = worldPos.setFromMatrixPosition(el.object3D.matrixWorld);
-          		el.object3D.position.set(newElPos);
+          		keepBallAttached = false;
         	}
       	});
+	},
+
+	/** Update ball on each tick or frame of the scene’s render loop **/
+	tock: function(time, timeDelta, evt) {
+		if (keepBallAttached == true) {
+			var camera = document.querySelector('#camera').object3D;
+		  	var cameraPos = camera.position;
+		  	var cameraRot = camera.rotation;
+		  	attachBallToCamera(el, cameraPos, cameraRot, evt, time, timeDelta);	
+	  	}
 	}
+
 });
